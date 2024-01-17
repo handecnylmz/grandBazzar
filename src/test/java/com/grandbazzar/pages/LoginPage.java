e066bcb2adbd9db08189fdd42798571261faabae
Zehra <zehradrc98@gmail.com>
2024-01-17 15:51:47 +0300

    Login page is edited

--- src/test/java/com/grandbazzar/pages/LoginPage.java
+++ src/test/java/com/grandbazzar/pages/LoginPage.java
@@ -1,4 +1,60 @@
 package com.grandbazzar.pages;
 
+import com.grandbazzar.utilities.ConfigurationReader;
+import com.grandbazzar.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+
+    public LoginPage(){
+
+        PageFactory.initElements(Driver.get(),this);
+    }
+
+    @FindBy(xpath = "//button[.='Join']")
+    public WebElement joinBtn;
+
+    @FindBy(xpath = "//div[@class='loginmodal_loginModalContent__VZSev']")
+    public WebElement loginBox;
+
+    @FindBy(xpath = "//input[@id='email']")
+    public WebElement emailBox;
+
+    @FindBy(xpath = "//input[@id='password']")
+    public WebElement passwordBox;
+
+    @FindBy(xpath = "//button[@value='submit']")
+    public WebElement loginBtn;
+
+    @FindBy(xpath = "//img[@alt='profile']")
+    public WebElement profileImg;
+
+    public void loginAsUser(){
+
+        emailBox.sendKeys(ConfigurationReader.getProperty("userEmail"));
+        passwordBox.sendKeys(ConfigurationReader.getProperty("userPassword"));
+        loginBtn.click();
+    }
+
+    // -------- ADMIN -----------
+    @FindBy(xpath = "//input[@name='email']")
+    public WebElement adminUsernameBox;  //input[@name='email']
+
+    @FindBy(xpath = "//input[@name='password']")
+    public WebElement adminPasswordBox;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement submitBtn;
+
+    @FindBy(xpath = "//a[.='Dashboard']")
+    public WebElement adminDashboardLink;
+
+    public void loginAsAdmin(){
+
+        adminUsernameBox.sendKeys(ConfigurationReader.getProperty("AdminEmail"));
+        adminPasswordBox.sendKeys(ConfigurationReader.getProperty("AdminPassword"));
+        submitBtn.click();
+    }
 }
