53cac687c9e6f7d1cce05c24df70862cbd292e94
Zehra <zehradrc98@gmail.com>
2024-02-02 19:45:08 +0300

    ChangePasswordPage' web elements are added

--- src/test/java/com/grandbazzar/pages/ChangePasswordPage.java
+++ src/test/java/com/grandbazzar/pages/ChangePasswordPage.java
@@ -1,4 +1,38 @@
 package com.grandbazzar.pages;
 
+import com.grandbazzar.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class ChangePasswordPage {
+
+    public ChangePasswordPage(){
+
+        PageFactory.initElements(Driver.get(),this);
+    }
+
+    @FindBy(xpath = "//button[.='Change Password']")
+    public WebElement changePasswordLink;
+
+    @FindBy(xpath = "//input[@id='oldPassword']")
+    public WebElement oldPasswordBox;
+
+    @FindBy(xpath = "//input[@id='newPassword']")
+    public WebElement newPasswordBox;
+
+    @FindBy(xpath = "//input[@id='confirmPassword']")
+    public WebElement confirmPasswordBox;
+
+    @FindBy(xpath = "//button[.='Submit']")
+    public WebElement submitButton;
+
+    @FindBy(xpath = "//span[.='Password changed successfully!']")
+    public WebElement changeSuccessfullyMsg;
+
+    @FindBy(xpath = "//label[.='Confirm Password']/following-sibling::span")
+    public WebElement confirmPassWarningMsg;
+
+
+
 }
